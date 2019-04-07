ade7bfebcf89c21cfa049f5623533a10a07e80ee
William Coelho <williamdavicoelho@gmail.com>
2019-04-07 17:11:39 -0300

    add dicionario estadosFinais

--- tabelaDirecionada.py
+++ tabelaDirecionada.py
@@ -2,7 +2,11 @@ entrada = {'a':12,'b':16,'c':5,'d':15,'e':3,'f':1,'g':16,'h':14,'i':0,'j':16,'k'
     'n':8,'o':11,'p':16,'q':16,'r':7,'s':2,'t':9,'u':13,'v':6,'w':4,'x':16,'y':16,'z':16,'0':17,
     '1':17,'2':17,'3':17,'4':17,'5':17,'6':17,'7':17,'8':17,'9':17,'/':18,'+':19,'-':20,'*':21,
     '<':22,'>':23,'=':24,'{':25,'}':26,'[':27,']':28,'!':29,'@':30,'.':31}
-    
+
+estadosFinais = {4:'int',10:'float',17:'struct',22:'else',24:'if',30:'while',35:'char',40:'void',
+47:'return',52:'comentario',54:'identificador',56:'+',58:'-',60:'*',63:'>',65:'<',66:'=',68:'>=',70:'<=',
+73:'==',76:'!=',80:'abreChave',78:'fechaChave',82:'abreColchete',84:'fechaColchete',86:'constNumerica'
+,89:'constNumerica'}    
 
 def geraTabelaDirecionada():
    # nomeArq = raw_input("\nNome do arquivo: ")
